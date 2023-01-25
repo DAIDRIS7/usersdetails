6a48eed6823b34bb5591ac9e07fe60790243a62f
Dfg_Dfgh <dd180841@gmail.com>
2023-01-25 11:30:48 +0000

    add MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:usersdetails/features/users/screens/users.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+import 'package:usersdetails/features/users/cubits/users_cubit.dart';
+
+void main() => runApp(
+      MultiProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AllUserDataCubit()..fetchAllUserData(),
+          ),
+        ],
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+    return MaterialApp(home: AllUser());
   }
 }
